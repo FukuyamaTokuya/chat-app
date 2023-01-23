b9409b462544f98ac0f40e1889d3574d324ec80b
FukuyamaTokuya <fukuya7711@gmail.com>
2023-01-23 22:05:33 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,32 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+ 
+   if Message.create(create_params)
+  
+    redirect_to room_messages_path(params[:room_id])
+   else
+    render :index
+    end
+    #別の書き方
+    # @room = Room.find(params[:room_id])
+    # @message = @room.messages.new(message_params)
+    # if @message.save
+    #   redirect_to room_messages_path(@room)
+    # else
+    #   render :index
+    # end
+
 
   end
+
+  private
+    def create_params
+      params.require(:message).permit(:content).merge(room_id: params[:room_id], user_id: current_user.id)
+    end
+
 end
